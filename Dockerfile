1963d761404b15d6a4fca5817c4f163eddc92aa9
Martin Beránek <martin.beranek@shipmonk.com>
2023-04-05 07:56:01 +0200

    ✨ Add init setup

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:latest AS build
+
+WORKDIR /opt
+COPY main.go /opt/main.go
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main main.go
+
+FROM scratch
+COPY --from=0 /opt/main /main
+COPY filler.jpg /filler.jpg
+
+ENV PORT 3000
+
+EXPOSE 3000/tcp
+
+ENTRYPOINT ["/main"]
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+// From https://gobyexample.com/http-servers
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func hi(w http.ResponseWriter, req *http.Request) {
+	b, _ := ioutil.ReadFile("./filler.jpg")
+	base64.StdEncoding.EncodeToString(b)
+	fmt.Fprintf(w, "<html>\n")
+	fmt.Fprintf(w, "<body>\n")
+	fmt.Fprintf(w, "<img src=\"data:image/jpeg;base64,%v\">\n", base64.StdEncoding.EncodeToString(b))
+	fmt.Fprintf(w, "</body>\n")
+	fmt.Fprintf(w, "</html>\n")
+	fmt.Println("hi called")
+}
+
+func main() {
+	http.HandleFunc("/", hi)
+
+	fmt.Println("Listening on port", os.Getenv("PORT"))
+	err := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
+	if err != nil {
+		panic(err)
+	}
+}
